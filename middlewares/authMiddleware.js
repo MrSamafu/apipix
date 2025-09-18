84038a1bffae4956a7995ecd1f66a0a01ff8bea2
MrSamafu <samson.flamme@hotmail.fr>
2025-09-18 22:04:47 +0200

    Authorization Token

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-    const token = req.header('Authorization');
+    const authHeader = req.header('Authorization');
+    const token = authHeader && authHeader.startsWith('Bearer ')
+        ? authHeader.split(' ')[1]
+        : authHeader;
     if (!token) return res.status(401).json({ error: 'Accès refusé' });
-    
+
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
         if (err) return res.status(403).json({ error: 'Token invalide' });
         req.user = user;
